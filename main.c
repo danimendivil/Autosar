978660dd0b66d8958aa3ca5d6fe0e66c85d007a5
Daniel Mendivil Byerly <danielmbyerly@hotmail.com>
2023-08-04 12:42:46 -0700

    usage of macros on main program

--- main.c
+++ main.c
@@ -1,14 +1,15 @@
+#include "Registers.h"
 int main( void )
 {
     /*enable GPIOA clock*/
-    *(volatile unsigned long*)0x40021034 |= 0x01;
+    RCC_GPIOA_CLK_EN();
     /*Set pin 5 from port A as output*/
-    *(volatile unsigned long*)0x50000000 &= ~(2 << (5 << 1));
+    PORTA->MODER &= ~(2 << (5 << 1));
     
     for( ; ; ) /*infinite loop*/
     {
         /*toggle pin 5 where the led is conected*/
-        *(volatile unsigned long*)0x50000014 ^= (1 << 5);
+        PORTA->ODR ^= (1 << 5);
         /*Dirty delay*/
         for( unsigned long i=0 ; i<100000 ; i++ );
     }
